2db587ad30e896c4115dec13adade677b289372c
hjh <ktorn21@naver.com>
2025-09-05 22:28:39 +0900

    초기설정

---
+++ vue-posts/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheHeader from './layouts/TheHeader.vue';
+import TheVue from './layouts/TheVue.vue';
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+  <TheVue></TheVue>
+  
+</template>
+
+<style scoped>
+
+</style>
