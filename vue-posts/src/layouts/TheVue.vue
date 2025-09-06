da88106e8540cd97665d40f90178e0a8d0da6e13
hjh <ktorn21@naver.com>
2025-09-06 21:44:52 +0900

    화면 초기 설정

--- vue-posts/src/layouts/TheVue.vue
+++ vue-posts/src/layouts/TheVue.vue
@@ -1,12 +1,14 @@
 <template>
     <main>
     <div class="container py-4">
-      <h2>Hello Vue3</h2>
+      <RouterView></RouterView>
     </div>
   </main>
 </template>
 
 <script setup>
+import { RouterView } from 'vue-router';
+
 
 </script>
 
